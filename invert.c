94da61248143377b59c904bc902e428228f500ff
Rafael <163610264+rafern13@users.noreply.github.com>
2024-09-09 20:43:43 -0400

    added while

--- invert.c
+++ invert.c
@@ -6,92 +6,103 @@ void mostrarMatriz(Fraction **matriz, int tamanho);
 
 int main()
 {
-  int tamanho;
-  printf("Defina o tamanho da matriz (deve ser quadrada): ");
-  scanf("%d", &tamanho);
+  while(1) {
+      printf("Bem vindo a calculadora de matriz invertida.\n");
+      int tamanho;
+      printf("Defina o tamanho da matriz (deve ser quadrada): ");
+      scanf("%d", &tamanho);
 
-  Fraction **matriz = (Fraction **)malloc(tamanho * sizeof(Fraction *));
-  for (int i = 0; i < tamanho; i++)
-    matriz[i] = (Fraction *)malloc(tamanho * 2 * sizeof(Fraction));
+      Fraction **matriz = (Fraction **)malloc(tamanho * sizeof(Fraction *));
+      for (int i = 0; i < tamanho; i++)
+        matriz[i] = (Fraction *)malloc(tamanho * 2 * sizeof(Fraction));
 
-  printf("Ótimo. Uma matriz %dx%d foi criada. Agora, vamos definir os valores dela.\n", tamanho, tamanho);
-  for (int i = 0; i < tamanho; i++)
-  {
-    for (int j = 0; j < tamanho; j++)
-    {
-      printf("Linha %d, coluna %d: ", i + 1, j + 1);
-      int n;
-      scanf("%d", &n);
-      Fraction f = {n, 1};
-      matriz[i][j] = f;
-    }
-  }
-
-  // cria matriz identidade
-  for (int i = 0; i < tamanho; i++)
-  {
-    for (int j = tamanho; j < tamanho * 2; j++)
-    {
-      Fraction f = {i == j - tamanho ? 1 : 0, 1};
-      matriz[i][j] = f;
-    }
-  }
-
-  // subtrai as equações para tirar os números à esquerda
-  for (int i = 0; i < tamanho - 1; i++)
-  {
-    Fraction n = matriz[i][i];
-    for (int j = i + 1; j < tamanho; j++)
-    {
-      Fraction n2 = matriz[j][i];
-      Fraction m = fsimplify(fdiv(n2, n));
-      for (int k = 0; k < tamanho * 2; k++)
+      printf("Ótimo. Uma matriz %dx%d foi criada. Agora, vamos definir os valores dela.\n", tamanho, tamanho);
+      for (int i = 0; i < tamanho; i++)
       {
-        Fraction r = fsimplify(fsub(matriz[j][k], fsimplify(fmul(matriz[i][k], m))));
-        matriz[j][k] = r;
+        for (int j = 0; j < tamanho; j++)
+        {
+          printf("Linha %d, coluna %d: ", i + 1, j + 1);
+          int n;
+          scanf("%d", &n);
+          Fraction f = {n, 1};
+          matriz[i][j] = f;
+        }
       }
-    }
-    printf("\n");
-    mostrarMatriz(matriz, tamanho);
-  }
 
-  // subtrai as equações para tirar os números à direita
-  for (int i = tamanho - 1; i > 0; i--)
-  {
-    Fraction n = matriz[i][i];
-    for (int j = i - 1; j >= 0; j--)
-    {
-      Fraction n2 = matriz[j][i];
-      Fraction m = fsimplify(fdiv(n2, n));
-      for (int k = 0; k < tamanho * 2; k++)
+      // cria matriz identidade
+      for (int i = 0; i < tamanho; i++)
       {
-        matriz[j][k] = fsimplify(fsub(matriz[j][k], fsimplify(fmul(matriz[i][k], m))));
+        for (int j = tamanho; j < tamanho * 2; j++)
+        {
+          Fraction f = {i == j - tamanho ? 1 : 0, 1};
+          matriz[i][j] = f;
+        }
       }
+
+      // subtrai as equações para tirar os números à esquerda
+      for (int i = 0; i < tamanho - 1; i++)
+      {
+        Fraction n = matriz[i][i];
+        for (int j = i + 1; j < tamanho; j++)
+        {
+          Fraction n2 = matriz[j][i];
+          Fraction m = fsimplify(fdiv(n2, n));
+          for (int k = 0; k < tamanho * 2; k++)
+          {
+            Fraction r = fsimplify(fsub(matriz[j][k], fsimplify(fmul(matriz[i][k], m))));
+            matriz[j][k] = r;
+          }
+        }
+        printf("\n");
+        mostrarMatriz(matriz, tamanho);
+      }
+
+      // subtrai as equações para tirar os números à direita
+      for (int i = tamanho - 1; i > 0; i--)
+      {
+        Fraction n = matriz[i][i];
+        for (int j = i - 1; j >= 0; j--)
+        {
+          Fraction n2 = matriz[j][i];
+          Fraction m = fsimplify(fdiv(n2, n));
+          for (int k = 0; k < tamanho * 2; k++)
+          {
+            matriz[j][k] = fsimplify(fsub(matriz[j][k], fsimplify(fmul(matriz[i][k], m))));
+          }
+        }
+        printf("\n");
+        mostrarMatriz(matriz, tamanho);
+      }
+
+      // normaliza a matriz
+      // em outras palavras, transforma a matriz da esquerda em identidade
+      for (int i = 0; i < tamanho; i++)
+      {
+        Fraction div = matriz[i][i];
+        for (int j = 0; j < tamanho * 2; j++)
+        {
+          matriz[i][j] = fsimplify(fdiv(matriz[i][j], div));
+          if (matriz[i][j].denominator == -0)
+            matriz[i][j].denominator = 0;
+        }
+      }
+
+      printf("\n");
+      mostrarMatriz(matriz, tamanho);
+
+      for (int i = 0; i < tamanho; i++)
+        free(matriz[i]);
+      free(matriz);
+    
+    char again;
+    printf("\n\nDeseja operar novamente? [s/n]");
+    scanf("%s", &again);
+    if (again == 'n' || again == 'N') {
+      return 0;
+    } else {
+      printf("\n");
     }
-    printf("\n");
-    mostrarMatriz(matriz, tamanho);
   }
-
-  // normaliza a matriz
-  // em outras palavras, transforma a matriz da esquerda em identidade
-  for (int i = 0; i < tamanho; i++)
-  {
-    Fraction div = matriz[i][i];
-    for (int j = 0; j < tamanho * 2; j++)
-    {
-      matriz[i][j] = fsimplify(fdiv(matriz[i][j], div));
-      if (matriz[i][j].denominator == -0)
-        matriz[i][j].denominator = 0;
-    }
-  }
-
-  printf("\n");
-  mostrarMatriz(matriz, tamanho);
-
-  for (int i = 0; i < tamanho; i++)
-    free(matriz[i]);
-  free(matriz);
-  return 0;
 }
 
 void mostrarMatriz(Fraction **matriz, int tamanho)
